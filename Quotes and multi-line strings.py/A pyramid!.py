b223df35d724315f22942d0cdaeaf28dfb08617e
Dante Taviantz <taviantz@gmail.com>
2022-07-24 11:45:37 -0700

    Printing four line strings.

---
+++ Quotes and multi-line strings.py/A pyramid!.py
@@ -0,0 +1,15 @@
+#Print a four-line string that includes:
+#One single quote;
+#One single quote, one double quote, and one single quote;
+#Five alternating single and double quotes, starting with a single quote;
+#Seven alternating single and double quotes, starting with a single quote.
+#Your output should look like this:
+#'
+#'"'
+#'"'"'
+#'"'"'"'
+
+print(""" '
+'"'
+'"'"'
+'"'"'"'""");
