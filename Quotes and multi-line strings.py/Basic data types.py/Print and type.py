2910ae4f0eb406c2a4ab4b0cc5051fdbbd817d03
Dante Taviantz <taviantz@gmail.com>
2022-07-26 12:48:36 -0700

    Writing a program that prints a string first and then its type.

---
+++ Quotes and multi-line strings.py/Basic data types.py/Print and type.py
@@ -0,0 +1,10 @@
+"""You want to write a program that prints a string first and then prints its type, in this particular order. The output should follow this pattern:
+This is a string
+<class 'str'>"""
+
+print('This is a string')
+print(type('This is a string'))
+
+print("This is a string")
+print(type("This is a string"))
+
