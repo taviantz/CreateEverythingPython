875569d2618330076c09cf5a3ff783c7dec65d77
Dante Taviantz <taviantz@gmail.com>
2022-07-26 20:02:43 -0700

    Creating a program that will print a multi-line string. Containing triple double quotes.

---
+++ Basic data types.py/Part of Project Show me a Animal.py/Show me an animal!.py
@@ -0,0 +1,44 @@
+"""Example
+Your output should contain the following ASCII image:
+
+Switching on the camera in the camel habitat...
+ ___.-''''-.
+/___  @    |
+',,,,.     |         _.'''''''._
+     '     |        /           \
+     |     \    _.-'             \
+     |      '.-'                  '-.
+     |                               ',
+     |                                '',
+      ',,-,                           ':;
+           ',,| ;,,                 ,' ;;
+              ! ; !'',,,',',,,,'!  ;   ;:
+             : ;  ! !       ! ! ;  ;   :;
+             ; ;   ! !      ! !  ; ;   ;,
+            ; ;    ! !     ! !   ; ;
+            ; ;    ! !    ! !     ; ;
+           ;,,      !,!   !,!     ;,;
+           /_I      L_I   L_I     /_I
+Look at that! Our little camel is sunbathing!"""
+
+x = r"""
+Switching on the camera in the camel habitat...
+ ___.-''''-.
+/___  @    |
+',,,,.     |         _.'''''''._
+     '     |        /           \
+     |     \    _.-'             \
+     |      '.-'                  '-.
+     |                               ',
+     |                                '',
+      ',,-,                           ':;
+           ',,| ;,,                 ,' ;;
+              ! ; !'',,,',',,,,'!  ;   ;:
+             : ;  ! !       ! ! ;  ;   :;
+             ; ;   ! !      ! !  ; ;   ;,
+            ; ;    ! !     ! !   ; ;
+            ; ;    ! !    ! !     ; ;
+           ;,,      !,!   !,!     ;,;
+           /_I      L_I   L_I     /_I
+Look at that! Our little camel is sunbathing!"""
+print(x)
---
+++ Quotes and multi-line strings.py/Triple-quoted string.py
@@ -0,0 +1,15 @@
+#Create a program that will print this multi-line string containing triple double quotes:
+#"""
+#THIS IS A STRING
+#"""
+
+# example 1
+x = '''"""
+THIS IS A STRING
+"""'''
+print(x)
+
+# example 2 
+print('"""')
+print("THIS IS A STRING")
+print('"""')
