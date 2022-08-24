6c4af2d80566ec44dca81dadec62adbcb93f1e60
Dante Taviantz <taviantz@gmail.com>
2022-08-24 16:05:25 -0700

    define a variable and assign a value to it? Mind that the value should be a str type.

--- Basic data types.py/Part of Project Show me a Animal.py/Project_Hangman.py/Hello, Hangman.py
+++
@@ -1,3 +0,0 @@
-# Write your code here
-print("H A N G M A N")
-print("The game will be available soon.")
---
+++ Basic data types.py/Part of Project Show me a Animal.py/variables.py/Define, assign, and use!.py
@@ -0,0 +1,3 @@
+#What is the correct way to define a variable and assign a value to it? Mind that the value should be a str type.    
+
+marvel_superhero = "Iron Man"
