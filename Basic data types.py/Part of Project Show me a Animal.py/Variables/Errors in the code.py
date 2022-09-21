675647266c452a2da360bd5a8a03e4e8b625b219
Dante Taviantz <taviantz@gmail.com>
2022-09-20 21:54:44 -0700

    Correcting errors in python code.

---
+++ Basic data types.py/Part of Project Show me a Animal.py/Variables/Errors in the code.py
@@ -0,0 +1,4 @@
+#Imagine your friend has created a code for checking the type of variable (ost = 'Happy'), but you have found some errors in it. Help him: find two or three errors, correct the code and print the type of the variable.
+
+ost = "Happy"
+print(type(ost))
