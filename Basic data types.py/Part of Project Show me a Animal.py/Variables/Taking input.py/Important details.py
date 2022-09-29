90cfc03dfd0eb6e5279f59666c0831262d203d09
Dante Taviantz <taviantz@gmail.com>
2022-09-28 19:20:22 -0700

    Converting a input to a string with a number.

---
+++ Basic data types.py/Part of Project Show me a Animal.py/Variables/Taking input.py/Important details.py
@@ -0,0 +1,7 @@
+#Any value you enter, the function sees as a string. 
+#It doesn't matter if you enter digits or letters. 
+#The input will be converted to a string.
+#If you want a number to be a number, you should write it explicitly.
+
+print("What's your favorite number?")
+value = int(input())  # now value keeps an integer number
---
+++ Basic data types.py/Part of Project Show me a Animal.py/Variables/Taking input.py/Several Inputs.py
@@ -0,0 +1,7 @@
+#To read several inputs you should call the function more than once. 
+day = int(input())  # 4
+month = input()     # October
+print('Cinnamon roll day is celebrated on', month, day)
+# Cinnamon roll day is celebrated on October 4
+#dont push this code to github.
+#I think its just an example without any code forming a result.
