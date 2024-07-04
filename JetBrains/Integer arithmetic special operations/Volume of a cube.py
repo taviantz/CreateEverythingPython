48444b7e7aee6af1def89a935985de660abbc456
Dante Taviantz <dante@dantetaviantz.com>
2024-07-04 08:53:02 -0700

    Add login feature

---
+++ JetBrains/Integer arithmetic special operations/Volume of a cube.py
@@ -0,0 +1,8 @@
+#Volume of a cube. A python lesson to calculate the volume of a cube. I am finding the volume of a cube.
+# Using the the simple formula: Volume = (side length)***3
+# My task is to simply apply the given formula Volume = (side length)***3 to find the volume of the cube and print it to the screen by writing a Python program.
+
+side_length = 4
+volume = side_length**3
+print(volume)
+
