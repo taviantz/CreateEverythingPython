ffcc54dfd0090617e4c7dfed02518a99aafb92d9
Dante Taviantz <dante@dantetaviantz.com>
2024-07-05 15:19:15 -0700

    Completed JetBrains lesson on finding correct operators to set compiler version to 1

--- JetBrains/Integer arithmetic special operations/TheRemainder.py
+++ JetBrains/Integer arithmetic special operations/TheRemainder.py
@@ -1,1 +1,6 @@
+#Python lesson. Integer arithmetic: special operations. 
+#The Remainder. Coding the compiler from the given code off jetbrains to 1. The operators used are -,//,*. 
+#I wrote the code into visual studio code. With the given operators that gave the result 1. 
+#code 
+
 print(7 - (7 // 3) * 3)
