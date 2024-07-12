f2b5eec363e8fc015a8b1abe16b843845d8cc79d
Dante Taviantz <dante@dantetaviantz.com>
2024-07-12 13:08:35 -0700

    Add Python program to find the last digit of a number

---
+++ JetBrains/Integer arithmetic special operations/the_last_digit.py
@@ -0,0 +1,32 @@
+# This Python program finds the last digit of a secret number
+
+# Step 1: Assign the secret number to a variable
+# In this example, we'll use 12345 as our secret number
+secret_number = 12345
+
+# Step 2: Use the modulus operator to find the last digit
+# The modulus operator is the percent sign (%) calculates the remainder of a division operation
+# When a number is divided by 10, the remainder is the last digit of that number
+# This is because 10 is the base of our number system 
+
+last_digit = secret_number % 10
+
+# Step 3: Print the result
+# This will display the last digit of the secret number 
+
+print(last_digit)
+
+
+# ---------------------------------------------------------
+# JetBrains Python version. 
+# Finding a Python program of the last digit of a secret number
+
+# Step 1: Assign the secret number to a variable
+# In this example, we'll use 12345 as our secret number
+x = 12345
+
+# Step 2: Use the modulus operator to find the last digit and print it directly
+# The modulus operator (%) calculates the remainder of a division operation
+# When a number is divided by 10, the remainder is the last digit of that number
+# This is because 10 is the base of our number system
+print(x % 10)
